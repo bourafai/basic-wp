a1ba064e985c78790909425fc419530e5fbbea87
Aymene Bourafai <bourafai.a@gmail.com>
2020-05-30 15:13:25 +0200

    feat (navigation) add navigation buttons with saving into session the state of post (titre and v))

--- content/plugins/ab-posts-render/assets/js/scripts.js
+++ content/plugins/ab-posts-render/assets/js/scripts.js
@@ -5,6 +5,7 @@ jQuery(document).ready(function ($) {
    * @param {string} v - The version of the template.
    */
   function appendFakeAds (v = 1) {
+    let appended = false
 
     /**
      * generate HTML markup to insert into the page
@@ -34,6 +35,7 @@ jQuery(document).ready(function ($) {
       
       .sidebar{
         float: right;
+        height:0px;
         width: 300px;
         color: #ce8888;
         position: relative;
@@ -58,6 +60,7 @@ jQuery(document).ready(function ($) {
       if (type == 'v2') {
         return `
       <style>
+      
       .sticky-ad.sticky-ad_bottom {
           top: unset;
           bottom: 0px !important;
@@ -72,7 +75,7 @@ jQuery(document).ready(function ($) {
       }
       
       </style>
-      <div class="ad ${type} ${cssClass}" id="${type}Ad"><img class="ad-content" src="https://dummyimage.com/600x180/006bb3/ffffff.png&text=${type}" alt=""></div>
+      <div style="opacity: 0.5" class="ad ${type} ${cssClass}" id="${type}Ad"><img class="ad-content" src="https://dummyimage.com/600x100/006bb3/ffffff.png&text=${type}" alt=""></div>
 `
       }
       if (type == 'v1') {
@@ -111,12 +114,14 @@ jQuery(document).ready(function ($) {
       $('article').on('disappear', function (event, $all_appeared_elements) {
         $('#v1Ad').hide('slow')
       })
+      return true
     }
 
     /*
     * add behavior for the v2 ad template
     */
     function handleAdV2 () {
+      console.log('handle v2')
       $('body').prepend(getAdMarkup('v2', 'sticky-ad'))
       $v2Ad = $('#v2Ad')
 
@@ -141,6 +146,7 @@ jQuery(document).ready(function ($) {
         }, 30))
 
       })
+      return true
     }
 
     /*
@@ -148,33 +154,64 @@ jQuery(document).ready(function ($) {
     */
     function handleAdV3 () {
       $('article').prepend(getAdMarkup('v3'))
-      $('article .entry-content').css({display: 'inline-block'})
+      $('article .entry-content,article .entry-header').css({display: 'inline-block'})
       $v3Ad = $('#sidebarAd')
       $v3Ad.stickySidebar({
         topSpacing: 60,
         bottomSpacing: 60
       })
+      return true
     }
 
     switch (v) {
-      case '1' : {
-        handleAdV1()
+      case 1 : {
+        appended = handleAdV1()
         break
       }
-      case '2' : {
-        handleAdV2()
+      case 2 : {
+        appended = handleAdV2()
         break
       }
-      case '3' : {
-        handleAdV3()
+      case 3 : {
+        appended = handleAdV3()
         break
       }
     }
+    return appended
+  }
+
+  /**
+   * http://stackoverflow.com/a/10997390/11236
+   */
+  function updateURLParameter (url, param, paramVal) {
+    var newAdditionalURL = ''
+    var tempArray = url.split('?')
+    var baseURL = tempArray[0]
+    var additionalURL = tempArray[1]
+    var temp = ''
+    if (additionalURL) {
+      tempArray = additionalURL.split('&')
+      for (var i = 0; i < tempArray.length; i++) {
+        if (tempArray[i].split('=')[0] != param) {
+          newAdditionalURL += temp + tempArray[i]
+          temp = '&'
+        }
+      }
+    }
+
+    var rows_txt = temp + '' + param + '=' + paramVal
+    return baseURL + '?' + newAdditionalURL + rows_txt
   }
 
   // our code here
   const queryString = window.location.search
   const urlParams = new URLSearchParams(queryString)
-  appendFakeAds(urlParams.get('v'))
+  if (urlParams.get('v') > 0) {
+    appendFakeAds(urlParams.get('v') * 1)
+  } else {
+    let randomAd = Math.floor(Math.random() * 3) + 1
+    appendFakeAds(randomAd)
+    window.history.replaceState('', '', updateURLParameter(window.location.href, 'v', randomAd))
+  }
 
 })
--- content/plugins/ab-posts-render/classes/Main.php
+++ content/plugins/ab-posts-render/classes/Main.php
@@ -3,6 +3,7 @@
 namespace AB\Posts_Render;
 require_once AB_POSTS_RENDER_DIR . 'classes/Singleton.php';
 
+
 /**
  * The purpose of the main class is to init all the plugin base code like :
  *  - Taxonomies
@@ -21,10 +22,15 @@ class Main {
 	use Singleton;
 
 	protected function init() {
-		add_action( 'init', [ $this, 'init_translations' ] );
+		add_action( 'init', [ $this, 'ab_init' ] );
+		add_action( 'wp_logout', [ $this, 'wp_logout' ] );
 		remove_action( 'wp_head', '_wp_render_title_tag', 1 );
 		add_action( 'wp_head', [ $this, 'render_title' ] );
 		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_script' ] );
+
+		add_filter( 'next_post_link', [ $this, 'adjacent_post_link' ], 10, 5 );
+		add_filter( 'previous_post_link', [ $this, 'adjacent_post_link' ], 10, 5 );
+//		add_filter( 'navigation_markup_template', [ $this, 'navigation_markup_template' ], 10, 2 );
 	}
 
 	public function enqueue_script() {
@@ -41,13 +47,23 @@ class Main {
 		wp_enqueue_style( 'ab-styles', AB_POSTS_RENDER_URL . '/assets/css/styles.css', [], AB_POSTS_RENDER_VERSION );
 	}
 
+	public function wp_logout() {
+		if ( isset( $_COOKIE[ session_name() ] ) ) {
+			session_unset();   // détruit les variables de session
+			session_destroy();
+		}
+	}
 
 	/**
 	 * Load the plugin translation
 	 */
-	public function init_translations() {
+	public function ab_init() {
 		// Load translations
 		load_plugin_textdomain( 'ab-posts-render', false, AB_POSTS_RENDER_PLUGIN_DIRNAME . '/languages' );
+
+		if ( ! session_id() ) {
+			@session_start();
+		}
 	}
 
 
@@ -58,13 +74,17 @@ class Main {
 		global $post;
 		$title_alternatives = get_field( 'alternative_titles' );
 
+		//set a default 'v' to the post
+		$_SESSION['navigation'][ $post->ID ]['v'] = empty( $_GET['v'] ) ? rand( 1, 3 ) : intval( $_GET['v'] );
 
 		if ( ! empty( $title_alternatives ) ) {
-			$printed_title = $this->get_printed_title( $title_alternatives, $post->post_title );
-
+			$printed_title = $this->get_printed_title( $title_alternatives, $post );
 			$this->print_title( $printed_title );
 			$post->post_title = esc_html( $printed_title );
 		} else {
+			$_GET['titre']                                = 1;
+			$_SESSION['navigation'][ $post->ID ]['titre'] = 1;
+
 			$this->print_title( $post->post_title );
 		}
 	}
@@ -85,22 +105,43 @@ class Main {
 	 *
 	 * @return string title to print
 	 */
-	private function get_printed_title( $title_alternatives, $post_title ) {
+	private function get_printed_title( $title_alternatives, $post ) {
+		$post_title                = $post->post_title;
 		$alternative_titles_values = array_values( $title_alternatives );
-		if ( ! empty( $_GET['titre'] ) ) {
+		if ( isset( $_GET['titre'] ) ) {
 			if ( $_GET['titre'] == 1 ) {
+				$_GET['titre']                                = 1;
+				$_SESSION['navigation'][ $post->ID ]['title'] = 1;
+
 				return $post_title;
 			} else {
 				$title_index = array_search( 'title_' . $_GET['titre'], array_keys( $title_alternatives ) );
 
 				if ( $title_index === false || empty( array_values( $title_alternatives )[ $title_index ] ) ) {
-					return $this->get_random_title( array_filter( $alternative_titles_values ), $post_title );
+					return $this->get_random_title( $title_alternatives, $post );
 				} else {
+					$_SESSION['navigation'][ $post->ID ]['titre'] = $_GET['titre'];
+
 					return $alternative_titles_values[ $title_index ];
+
 				}
 			}
 		} else {
-			return $this->get_random_title( array_filter( $alternative_titles_values ), $post_title );
+			if ( ! empty( $_GET['nav'] ) && ! empty( $_SESSION['navigation'][ $post->ID ]['titre'] ) ) {
+				$titre       = $_SESSION['navigation'][ $post->ID ]['titre'];
+				$title_index = array_search( 'title_' . $titre, array_keys( $title_alternatives ) );
+				if ( $title_index === false ) {
+					return $this->get_random_title( $title_alternatives, $post );
+				} else {
+					return $alternative_titles_values[ $title_index ];
+				}
+			} else {
+				if ( empty( $_GET['nav'] ) ) {
+					$_SESSION['navigation'] = null;
+				}
+
+				return $this->get_random_title( $title_alternatives, $post );
+			}
 		}
 	}
 
@@ -112,13 +153,25 @@ class Main {
 	 *
 	 * @return string post title
 	 */
-	private function get_random_title( $alternative_titles, $post_title = '' ) {
-		$titles = array_values( $alternative_titles );
+	private function get_random_title( $alternative_titles, $post ) {
+		$post_title = $post->post_title;
+		$titles     = array_values( array_filter( $alternative_titles ) );
 		if ( ! empty( $post_title ) ) {
 			$titles[] = $post_title;
 		}
+		$title_index = rand( 0, sizeof( $titles ) - 1 );
+		$title_key   = array_search( $titles[ $title_index ], $alternative_titles, true );
+		$title_key   = (int) filter_var( $title_key, FILTER_SANITIZE_NUMBER_INT );
+		if ( ! empty( $title_key ) ) {
+			$_GET['titre']                                = $title_key;
+			$_SESSION['navigation'][ $post->ID ]['titre'] = $title_key;
+		} else {
+			$_GET['titre']                                = 1;
+			$_SESSION['navigation'][ $post->ID ]['titre'] = 1;
 
-		return $titles[ rand( 0, sizeof( $titles ) - 1 ) ];
+		}
+
+		return $titles[ $title_index ];
 	}
 
 	/**
@@ -130,6 +183,52 @@ class Main {
 		if ( did_action( 'wp_head' ) || doing_action( 'wp_head' ) ) {
 			echo '<title>' . esc_html( $title ) . '</title>' . "\n";
 		}
+	}
+
+	/**
+	 * Filters the adjacent post link.
+	 *
+	 * The dynamic portion of the hook name, `$adjacent`, refers to the type
+	 * of adjacency, 'next' or 'previous'.
+	 *
+	 * @since 2.6.0
+	 * @since 4.2.0 Added the `$adjacent` parameter.
+	 *
+	 * @param string $output The adjacent post link.
+	 * @param string $format Link anchor format.
+	 * @param string $link Link permalink format.
+	 * @param WP_Post $post The adjacent post.
+	 * @param string $adjacent Whether the post is previous or next.
+	 */
+	public function adjacent_post_link( $output, $format, $link, $adjacent_post, $adjacent ) {
+
+		global $post;
+		//save current post markup_version
+		$_SESSION['navigation'][ $post->ID ]['v'] = ! empty( $_GET['v'] ) ? $_GET['v'] : rand( 1, 3 );
+
+
+		if ( $adjacent_post ) {
+
+			$query_args['nav'] = true; // activate saving
+			//if adjacent post doesn't have a 'v' set => use random
+			$query_args['v'] = empty( $_SESSION['navigation'][ $adjacent_post->ID ]['v'] ) ? rand( 1, 3 ) : $_SESSION['navigation'][ $adjacent_post->ID ]['v'];
+
+			$adjacent_post_link = get_permalink( $adjacent_post );
+			$adjacent_post_link = add_query_arg( $query_args, $adjacent_post_link );
+
+			$link_class = $adjacent === 'next' ? 'btn__primary' : 'btn__secondary';
+			$rel        = $adjacent === 'previous' ? 'prev' : 'next';
+			$string     = '<a class="btn btn-rounded ' . $link_class . '" href="' . $adjacent_post_link . '" rel="' . $rel . '">';
+
+			$inlink = $string . $adjacent . '</a>';
+
+			$output = str_replace( '%link', $inlink, $format );
+
+			return preg_replace( '/href=".*?"/', 'href="' . esc_url( $adjacent_post_link ) . '"', $output );
+		}
+
+		//replace default URL
+
 	}
 
 }
